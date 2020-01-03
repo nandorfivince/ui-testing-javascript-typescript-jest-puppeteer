3079853afc3a322f81243d2a3c947abb21365c4f
Vince Burka <vburka@chemaxon.com>
2020-01-03 14:55:38 +0100

    some helpers added and exmaple test code refactored using helpers

--- tests/example.js
+++ tests/example.js
@@ -1,21 +1,30 @@
 const puppeteer = require('puppeteer')
 const expect = require('chai')
 
+const config = require('../lib/config')
+const click = require('../lib/helpers').click
+const typeText = require('../lib/helpers').typeText
+const loadUrl = require('../lib/helpers').loadUrl
+const waitForText  = require('../lib/helpers').waitForText
+const pressKey  = require('../lib/helpers').pressKey
+const shouldExsist  = require('../lib/helpers').shouldExsist
+
 describe('My first puppeteer test', () => {
     let browser // give a browser instance
     let page // gives methods interacts with browser
 
     before(async function(){
         browser = await puppeteer.launch({
-            headless: true,
-            sloMo: 0, // delay between each puppeteer actions
-            devtools: false,
-            timeout: 1000,
+            headless: config.isHeadless,
+            sloMo: config.slowMo, // delay between each puppeteer actions
+            devtools: config.isDevtools,
+            timeout: config.launchTimeout,
         })
         page = await browser.newPage()
+        await page.setDefaultTimeout(config.waitingTimeout)
         await page.setViewport({
-            width: 800,
-            height: 600,
+            width: config.viewportWidth,
+            height: config.viewportHeight,
         })
     })
 
@@ -25,8 +34,11 @@ describe('My first puppeteer test', () => {
 
 
     it('My first test step', async ()=> {
-        await page.goto("https://dev.to/")
-        await page.waitForSelector("#nav-search")
+        // await page.goto("https://dev.to/")
+        // await page.goto(config.baseUrl)
+        await loadUrl(page, config.baseUrl)
+        // await page.waitForSelector("#nav-search")
+        await shouldExsist(page, "#nav-search")
         const url = await page.url()
         const title = await page.title()
         // expect(url).to.contain("dev")
@@ -35,7 +47,10 @@ describe('My first puppeteer test', () => {
 
     it('browser reload', async () => {
         await page.reload()
-        await page.waitForSelector('#page-content')
+        // await page.waitForSelector('#page-content')
+        await shouldExsist(page, "#page-content")
+        await waitForText(page, 'body', "WRITE A POST")
+
         const url = await page.url()
         const title = await page.title()
         // expect(url).to.contain("dev")
@@ -43,21 +58,28 @@ describe('My first puppeteer test', () => {
     })
 
     it('click method', async () => {
-        await page.goto("https://dev.to/")
-        await page.waitForSelector("#write-link")
-        await page.click("#write-link", { // click funtion with options
-            button: "left",
-            clickCount: 2,
-            delay: 100
-        })
-        await page.waitForSelector(".registration-rainbow")
+        // await page.goto("https://dev.to/")
+        await loadUrl(page, config.baseUrl)
+        // await page.waitForSelector("#write-link")
+        // await page.click("#write-link", { // click funtion with options
+        //     button: "left",
+        //     clickCount: 2,
+        //     delay: 100
+        // })
+        await click(page, "#write-link")
+        // await page.waitForSelector(".registration-rainbow")
+        await shouldExsist(page, ".registration-rainbow")
     })
 
     it('submit into searchbox', async () => {
-        await page.goto("https://dev.to/")
-        await page.waitForSelector("#nav-search")
-        await page.type("#nav-search", "javascript")
-        await page.keyboard.press("Enter")
+        // await page.goto("https://dev.to/")
+        await loadUrl(page, config.baseUrl)
+        // await page.waitForSelector("#nav-search")
+        // await page.type("#nav-search", "javascript")
+        await typeText(page, "javascript", "#nav-search")
+        // await page.keyboard.press("Enter")
+        await pressKey(page, "Enter")
+        await page.waitForXPath('//*[@id="articles-list"]')
     })
 
 })
